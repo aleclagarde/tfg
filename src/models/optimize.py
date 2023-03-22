954e844034b1ec9c6175c15b99fec727ea43d802
alagarde <aleclagarde@gmail.com>
2023-03-23 00:57:28 +0100

    DEV: Updated all models to their correct form DEV: Adapted it for TensorFlow

--- src/models/optimize.py
+++ src/models/optimize.py
@@ -1,73 +1,88 @@
 from transformers import (
     BertTokenizer,
     BertModel,
+    TFBertModel,
     GPT2Tokenizer,
-    GPT2LMHeadModel,
+    GPT2Model,
+    TFGPT2Model,
     T5Tokenizer,
     T5ForConditionalGeneration,
+    TFT5ForConditionalGeneration,
     ViTFeatureExtractor,
     ViTForImageClassification,
-    OwlViTProcessor,
-    OwlViTForObjectDetection,
-    MaskFormerForInstanceSegmentation,
-    MaskFormerFeatureExtractor,
+    TFViTForImageClassification,
+    CLIPProcessor,
+    CLIPModel,
+    TFCLIPModel,
+    SegformerFeatureExtractor,
+    SegformerForSemanticSegmentation,
+    TFSegformerForSemanticSegmentation,
     AutoTokenizer,
     AutoModelForCausalLM,
+    TFAutoModelForCausalLM,
     AutoModelForMaskedLM,
+    TFAutoModelForMaskedLM,
 )
 import torch
 from torch.nn.utils import prune
 import tensorflow as tf
 import tensorflow_model_optimization as tfmot
 
-# from optimize_utils import prune_torch
-
 # Define the models and their associated tokenizers
 models = [
     {
         "name": "bert",
         "tokenizer": BertTokenizer.from_pretrained("bert-base-uncased"),
-        "model": BertModel.from_pretrained("bert-base-uncased"),
+        "model_torch": BertModel.from_pretrained("bert-base-uncased"),
+        "model_tf": TFBertModel.from_pretrained("bert-base-uncased"),
     },
     {
         "name": "gpt2",
         "tokenizer": GPT2Tokenizer.from_pretrained("gpt2"),
-        "model": GPT2LMHeadModel.from_pretrained("gpt2"),
+        "model_torch": GPT2Model.from_pretrained("gpt2"),
+        "model_tf": TFGPT2Model.from_pretrained("gpt2"),
     },
     {
         "name": "t5",
-        "tokenizer": T5Tokenizer.from_pretrained("t5-small"),
-        "model": T5ForConditionalGeneration.from_pretrained("t5-small"),
+        "tokenizer": T5Tokenizer.from_pretrained("t5-base"),
+        "model_torch": T5ForConditionalGeneration.from_pretrained("t5-base"),
+        "model_tf": TFT5ForConditionalGeneration.from_pretrained("t5-base"),
     },
     {
         "name": "vit",
         "tokenizer": ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224"),
-        "model": ViTForImageClassification.from_pretrained("google/vit-base-patch16-224"),
+        "model_torch": ViTForImageClassification.from_pretrained("google/vit-base-patch16-224"),
+        "model_tf": TFViTForImageClassification.from_pretrained("google/vit-base-patch16-224"),
     },
     {
-        "name": "owlvit",
-        "tokenizer": OwlViTProcessor.from_pretrained("google/owlvit-base-patch32"),
-        "model": OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32"),
+        "name": "clip",
+        "tokenizer": CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14"),
+        "model_torch": CLIPModel.from_pretrained("openai/clip-vit-large-patch14"),
+        "model_tf": TFCLIPModel.from_pretrained("openai/clip-vit-large-patch14"),
     },
     {
-        "name": "maskformer",
-        "tokenizer": MaskFormerFeatureExtractor.from_pretrained("facebook/maskformer-swin-small-coco"),
-        "model": MaskFormerForInstanceSegmentation.from_pretrained("facebook/maskformer-swin-small-coco"),
+        "name": "segformer",
+        "tokenizer": SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512"),
+        "model_torch": SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512"),
+        "model_tf": TFSegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512"),
     },
     {
         "name": "codeparrot",
         "tokenizer": AutoTokenizer.from_pretrained("codeparrot/codeparrot-small"),
-        "model": AutoModelForCausalLM.from_pretrained("codeparrot/codeparrot-small"),
+        "model_torch": AutoModelForCausalLM.from_pretrained("codeparrot/codeparrot-small"),
+        "model_tf": TFAutoModelForCausalLM.from_pretrained("codeparrot/codeparrot-small"),
     },
     {
         "name": "codeberta",
         "tokenizer": AutoTokenizer.from_pretrained("huggingface/CodeBERTa-small-v1"),
-        "model": AutoModelForMaskedLM.from_pretrained("huggingface/CodeBERTa-small-v1"),
+        "model_torch": AutoModelForMaskedLM.from_pretrained("huggingface/CodeBERTa-small-v1"),
+        "model_tf": TFAutoModelForMaskedLM.from_pretrained("huggingface/CodeBERTa-small-v1"),
     },
     {
         "name": "codegen",
         "tokenizer": AutoTokenizer.from_pretrained("Salesforce/codegen-350M-mono"),
-        "model": AutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono"),
+        "model_torch": AutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono"),
+        "model_tf": TFAutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono"),
     },
 ]
 
@@ -81,7 +96,8 @@ pruning_methods = [
 # Loop over the models and the coefficients and prune each model
 for model_dict in models:
     model_name = model_dict["name"]
-    model = model_dict["model"]
+    model_torch = model_dict["model_torch"]
+    model_tf = model_dict["model_tf"]
     tokenizer = model_dict["tokenizer"]
     for method_name, method_func in pruning_methods:
         for cf in pruning_cf:
@@ -89,28 +105,31 @@ for model_dict in models:
             if method_name == "torch":
                 pruning_method = method_func
                 # Get a list of all the modules in the model
-                modules = list(model.modules())
+                modules = list(model_torch.modules())
 
                 # Loop through each module and prune its parameters if they exist
                 for module in modules:
-                    if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
+                    print(type(module))
+                    if isinstance(module, torch.nn.modules.linear.Linear) or \
+                            isinstance(module, transformers.pytorch_utils.Conv1D):
                         pruning_method(module, name='weight', amount=cf)
 
-                # model = prune_torch(model_name, model, pruning_method, cf)
+                # Save the pruned model to disk
+                # model_torch.save_pretrained(f"{model_name}-{method_name}-pruned-{cf}")
             elif method_name == "tf":
-                continue
                 pruning_method = method_func
                 # Prune the model with the given coefficient
-                model = pruning_method(
-                    model,
+                model_tf = pruning_method(
+                    model_tf,
                     sparsity=cf,
                 )
 
                 # `prune_low_magnitude` requires a recompile.
-                model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                model_tf.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                               metrics=['accuracy'])
-            # Save the pruned model to disk
-            model.save_pretrained(f"{model_name}-{method_name}-pruned-{cf}")
+
+                # Save the pruned model to disk
+                # model_tf.save_pretrained(f"{model_name}-{method_name}-pruned-{cf}")
 
     # Optionally, you can reload the original model from disk
     # model_dict["model"] = model.from_pretrained(f"{model_name}-{method_name}-pruned-{pruning_cf[0]}")
