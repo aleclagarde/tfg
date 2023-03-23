a839d9d63ea6ac323e93bc9e4408a78a4f7d51d7
alagarde <aleclagarde@gmail.com>
2023-03-23 21:27:52 +0100

    DEV: Changed codegen to codegpt and finished script

--- src/models/optimize.py
+++ src/models/optimize.py
@@ -23,10 +23,13 @@ from transformers import (
     AutoModelForMaskedLM,
     TFAutoModelForMaskedLM,
 )
+import transformers
 import torch
 from torch.nn.utils import prune
 import tensorflow as tf
 import tensorflow_model_optimization as tfmot
+from codecarbon import EmissionsTracker
+import json
 
 # Define the models and their associated tokenizers
 models = [
@@ -70,7 +73,7 @@ models = [
         "name": "codeparrot",
         "tokenizer": AutoTokenizer.from_pretrained("codeparrot/codeparrot-small"),
         "model_torch": AutoModelForCausalLM.from_pretrained("codeparrot/codeparrot-small"),
-        "model_tf": TFAutoModelForCausalLM.from_pretrained("codeparrot/codeparrot-small"),
+        "model_tf": TFAutoModelForCausalLM.from_pretrained("codeparrot/codeparrot-small", from_pt=True),
     },
     {
         "name": "codeberta",
@@ -79,10 +82,10 @@ models = [
         "model_tf": TFAutoModelForMaskedLM.from_pretrained("huggingface/CodeBERTa-small-v1"),
     },
     {
-        "name": "codegen",
-        "tokenizer": AutoTokenizer.from_pretrained("Salesforce/codegen-350M-mono"),
-        "model_torch": AutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono"),
-        "model_tf": TFAutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono"),
+        "name": "codegpt",
+        "tokenizer": GPT2Tokenizer.from_pretrained("microsoft/CodeGPT-small-py"),
+        "model_torch": GPT2Model.from_pretrained("microsoft/CodeGPT-small-py"),
+        "model_tf": TFGPT2Model.from_pretrained("microsoft/CodeGPT-small-py"),
     },
 ]
 
@@ -92,6 +95,10 @@ pruning_methods = [
     ("torch", torch.nn.utils.prune.random_unstructured),
     ("tf", tfmot.sparsity.keras.prune_low_magnitude),
 ]
+emissions_dict = {
+    'pruning': {'torch': {}, 'tf': {}},
+    'quantization': {'torch': {}, 'tf': {}}
+}
 
 # Loop over the models and the coefficients and prune each model
 for model_dict in models:
@@ -107,29 +114,44 @@ for model_dict in models:
                 # Get a list of all the modules in the model
                 modules = list(model_torch.modules())
 
-                # Loop through each module and prune its parameters if they exist
+                # Loop through each module and prune its parameters if they exist while measuring emissions
+                tracker = EmissionsTracker()
+                tracker.start()
                 for module in modules:
-                    print(type(module))
                     if isinstance(module, torch.nn.modules.linear.Linear) or \
                             isinstance(module, transformers.pytorch_utils.Conv1D):
                         pruning_method(module, name='weight', amount=cf)
+                emissions: float = tracker.stop()
+                emissions_dict['pruning']['torch'][model_name] = emissions
 
                 # Save the pruned model to disk
-                # model_torch.save_pretrained(f"{model_name}-{method_name}-pruned-{cf}")
+                model_torch.save_pretrained(f"{model_name}-{method_name}-pruned-{cf}")
             elif method_name == "tf":
                 pruning_method = method_func
-                # Prune the model with the given coefficient
-                model_tf = pruning_method(
-                    model_tf,
-                    sparsity=cf,
-                )
+                # Get a list of all the modules in the model
+                model_tf = TFBertModel.from_pretrained("bert-base-uncased")
+                submodules = model_tf.submodules  # Access the submodules tuple as an attribute
+                modules = list(submodules)  # Convert the submodules tuple to a list
+
+                # Loop through each module and prune its parameters if they exist while measuring emissions
+                tracker = EmissionsTracker()
+                tracker.start()
+                for module in modules:
+                    if isinstance(module, tf.keras.models.Sequential):
+                        # Prune the model with the given coefficient
+                        pruning_method(model_tf, sparsity=cf)
+                emissions: float = tracker.stop()
+                emissions_dict['pruning']['tf'][model_name] = emissions
 
                 # `prune_low_magnitude` requires a recompile.
                 model_tf.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                              metrics=['accuracy'])
+                                 metrics=['accuracy'])
 
                 # Save the pruned model to disk
-                # model_tf.save_pretrained(f"{model_name}-{method_name}-pruned-{cf}")
+                model_tf.save_pretrained(f"{model_name}-{method_name}-pruned-{cf}")
+
+with open("optimization_emissions.json", "w") as outfile:
+    json.dump(emissions_dict, outfile)
 
     # Optionally, you can reload the original model from disk
     # model_dict["model"] = model.from_pretrained(f"{model_name}-{method_name}-pruned-{pruning_cf[0]}")
