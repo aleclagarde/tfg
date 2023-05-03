de92bd092d4a49e1381beb2bd134cbe51822fc83
alagarde <aleclagarde@gmail.com>
2023-05-04 01:34:18 +0200

    DEV: Added gpt2 inference (correctness not yet). DEV: Adapted to run n iterations of inference.

--- src/inference.py
+++ src/inference.py
@@ -1,37 +1,30 @@
 import pandas as pd
 import os
 
-from inference_functions import bert, t5
-from inference_utils import load_model
+from inference_functions import inference
+from inference_utils import add_measurements
 
 
-models = ['bert', 't5']
-data_size = 1
+models = ['gpt2']
+number_of_measurements = 1
 
 df = pd.DataFrame(columns=['timestamp', 'project_name', 'run_id', 'duration', 'emissions', 'emissions_rate',
                            'cpu_power', 'gpu_power', 'ram_power', 'cpu_energy', 'gpu_energy', 'ram_energy',
                            'energy_consumed', 'country_name', 'country_iso_code', 'region', 'cloud_provider',
                            'cloud_region', 'os', 'python_version', 'cpu_count', 'cpu_model', 'gpu_count', 'gpu_model',
                            'longitude', 'latitude', 'ram_total_size', 'tracking_mode', 'on_cloud', 'domain', 'model',
-                           'data_number', 'correct'])
+                           'iteration', 'correctness'])
+
 
 for model_short_name in models:
     models_suffix = ['-torch-baseline', '-torch-pruned', '-torch-quantized.pth', '-tf-baseline', '-tf-pruned',
                      '-tf-quantized']
     for suf in models_suffix:
         model_name = model_short_name + suf
-
-        print("#############################################################################################")
-        print(f'Inference for {model_name}')
-        print("#############################################################################################")
-
-        model, framework = load_model(model_short_name, 'models/saved/'+model_name)
-
-        if model_short_name == 'bert':
-            bert(data_size)
-        elif model_short_name == 't5':
-            df = t5(model_name=model_name, model=model, framework=framework, data_size=data_size, df=df)
-
+        for i in range(number_of_measurements):
+            correct = inference(model_name, model_short_name, number_of_measurements)
+            df = add_measurements(df, number_of_measurements=number_of_measurements, model_name=model_name,
+                                  iteration=i+1, correctness=False)
 
 df.to_csv('inference_results.csv')
 
--- src/inference_functions.py
+++ src/inference_functions.py
@@ -1,25 +1,90 @@
-import pandas as pd
+import numpy as np
 import torch
 import tensorflow as tf
 from datasets import load_dataset
-from transformers import T5Tokenizer
 from codecarbon import track_emissions
-from inference_utils import add_measurements
+from transformers import pipeline, GPT2Tokenizer, T5Tokenizer
 import time
 import itertools
-
-tokenizer = T5Tokenizer.from_pretrained("t5-base")
-
-
-def bert(data_size):
-    bert_dataset = load_dataset("bertin-project/mc4-es-sampled", 'gaussian', split="validation", streaming=True)
-    bert_dataset = itertools.islice(bert_dataset, data_size)
-    for text in bert_dataset:
-        print(text)
+from inference_utils import load_model
 
 
 @track_emissions
-def infer_t5(model, framework, text, quantized):
+def inference(model_name: str, model_short_name: str, number_of_measurements: int):
+    print("#############################################################################################")
+    print(f'Inference for {model_name}')
+    print("#############################################################################################")
+
+    model, framework = load_model(model_short_name, 'models/saved/' + model_name)
+
+    correct = []
+    if model_short_name == 'gpt2':
+        correct = gpt2(model_name=model_name, model=model, framework=framework,
+                       number_of_measurements=number_of_measurements)
+    elif model_short_name == 't5':
+        correct = t5(model_name=model_name, model=model, framework=framework,
+                     number_of_measurements=number_of_measurements)
+    return correct
+
+
+def infer_gpt2(text: str, model, tokenizer, framework: str, quantized: bool, length: int = 30,
+               temperature: float = 0.7) -> str:
+    if framework == 'pt':
+        generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+        output = generator(text, max_length=length, do_sample=True, temperature=temperature)
+
+        return output[0]["generated_text"]
+    else:
+        if quantized:
+            input_ids = tokenizer.encode(text, add_special_tokens=True)
+            input_ids = np.array(input_ids)[np.newaxis, :]
+            # Perform inference on the preprocessed input
+            output = model.predict(input_ids)
+            output_ids = np.argmax(output, axis=-1)
+            output_str = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+            return text + output_str
+        else:
+            prompt_tokens = tokenizer.encode(text)
+            input_seq = tf.constant([prompt_tokens])
+
+            output_seq = input_seq
+            for i in range(length):
+                logits = model({'input_ids': output_seq})['logits'][:, -1, :]
+                logits /= temperature
+                probs = tf.nn.softmax(logits).numpy()[0]
+                selected_token = np.random.choice(tokenizer.vocab_size, p=probs)
+                output_seq = tf.concat([output_seq, [[selected_token]]], axis=-1)
+
+            output_text = tokenizer.decode(output_seq.numpy()[0])
+
+            return output_text.strip()
+
+
+def gpt2(model_name: str, model, framework: str, number_of_measurements: int):
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+    # gpt2_dataset = load_dataset("openwebtext", split='test', streaming=True)
+    # gpt2_dataset = itertools.islice(gpt2_dataset, data_size)
+    gpt2_dataset = [{'text': "April is the fourth month"}]
+
+    i = 0
+    correct = []
+    for data in gpt2_dataset:
+        text = data['text']
+        start_time = time.time()
+        output = infer_gpt2(text=text, model=model, tokenizer=tokenizer, framework=framework,
+                            quantized='quantized' in model_name)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("#############################################################################################")
+        print(f'({i + 1}/{number_of_measurements}) Output : {output} Time taken: {elapsed_time} seconds')
+        correct.append(False)
+        i = i + 1
+    return correct
+
+
+def infer_t5(text: str, model, tokenizer, framework: str, quantized: bool) -> str:
     if quantized:
         if framework == 'pt':
             # Encode the input text using the T5 tokenizer
@@ -63,27 +128,30 @@ def infer_t5(model, framework, text, quantized):
         return translated_text.strip()
 
 
-def t5(model_name: str, model, framework: str, data_size: int, df: pd.DataFrame):
+def t5(model_name: str, model, framework: str, number_of_measurements: int):
+    tokenizer = T5Tokenizer.from_pretrained("t5-base")
+
     t5_dataset = load_dataset("opus100", "de-en", split="test", streaming=True)
-    t5_dataset = itertools.islice(t5_dataset, data_size)
+    t5_dataset = itertools.islice(t5_dataset, number_of_measurements)
 
     i = 0
+    correct = []
     for data in t5_dataset:
         sentence = data['translation']['en']
         target = data['translation']['de']
         # Translate text
-        start_time = time.time()
         prefix = "translate English to German: "
-        output = infer_t5(model=model, framework=framework, text=prefix+sentence, quantized='quantized' in model_name)
+        start_time = time.time()
+        output = infer_t5(text=prefix+sentence, model=model, tokenizer=tokenizer, framework=framework,
+                          quantized='quantized' in model_name)
         end_time = time.time()
         elapsed_time = end_time - start_time
         output = output.replace('"', '')
         target = target.replace('"', '')
         print("#############################################################################################")
-        print(f'({i + 1}/{data_size})Translated_text : {output} Target: {target} Time taken: {elapsed_time} seconds')
+        print(f'({i + 1}/{number_of_measurements}) Translated_text : {output} Target: {target} '
+              f'Time taken: {elapsed_time} seconds')
 
-        correct = output == target
-        df = add_measurements(df, number_of_measurements=1, model_name=model_name, data_number=i + 1, correct=correct)
+        correct.append(output == target)
         i = i + 1
-
-    return df
+    return correct
